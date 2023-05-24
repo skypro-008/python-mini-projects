602f942e2a4be7af6b78dfbddc98ef0eed6d7b77
kirillskyeng <kirill.netreba@skyeng.ru>
2023-05-24 11:57:43 +0300

    fix generator module

--- password_generator/generator.py
+++ password_generator/generator.py
@@ -1,24 +1,50 @@
 import random
 import string
 
-def stretch(text,maxlength):
-    if len(text) < maxlength:
-        randomChar = get_random_char()
-        return stretch(text+randomChar,maxlength)
+
+def stretch(text: str, max_len: int) -> str:
+    """
+    Растягивает заданный текст до указанной максимальной длины, добавляя случайные
+    символы в конец.
+
+    Args:
+        text: Текст для растяжения
+        max_len: Максимальная длина возвращаемой строки
+
+    Returns:
+        Растянутый текст
+    """
+    if len(text) < max_len:
+        random_char = get_random_char()
+        return stretch(text + random_char, max_len)
     else:
         return text
 
-def get_random_char():
-    chars = string.printable
-    randomChar = chars[random.randint(0,len(chars)-1)]
-    return randomChar
 
-while 1:
-    maxlen = input(' [?] Enter a length for your password (e for exit): ')
-    try:
-        maxlength = int(maxlen)
-        print("'",stretch('',maxlength),"'\n")
-    except:
-        if maxlen == 'e':
-            break
-        print('Please Enter an integer')
+def get_random_char() -> str:
+    """
+    Возвращает случайно выбранный символ из печатаемых ASCII-символов.
+    """
+    chars = string.printable
+    random_char = chars[random.randint(0, len(chars) - 1)]
+    return random_char
+
+
+def main() -> None:
+    """
+    Запрашивает у пользователя максимальную длину и повторяет растяжение пустой строки
+    до тех пор, пока она не достигнет этой длины. Печатает полученную строку.
+    """
+    while 1:
+        max_len = input(' [?] Enter a length for your password (e for exit): ')
+        try:
+            maxlength = int(max_len)
+            print("'", stretch('', maxlength), "'\n")
+        except ValueError:
+            if max_len == 'e':
+                break
+            print('Please Enter an integer')
+
+
+if __name__ == '__main__':
+    main()
