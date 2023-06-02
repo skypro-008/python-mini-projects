87c9cfb63b19cc918b35571d8845982ecd07707c
kirillskyeng <kirill.netreba@skyeng.ru>
2023-06-02 11:47:46 +0300

    Add code for lesson-5

---
+++ src/converter.py
@@ -0,0 +1,20 @@
+import json
+
+
+def json_to_csv():
+    try:
+        with open('input.json', 'r') as f:
+            data = json.loads(f.read())
+
+        output = ','.join([*data[0]])
+        for obj in data:
+            output += f'\n{obj["Name"]},{obj["age"]},{obj["birthyear"]}'
+
+        with open('output.csv', 'w') as f:
+            f.write(output)
+    except Exception as ex:
+        print(f'Error: {str(ex)}')
+
+
+if __name__ == '__main__':
+    json_to_csv()
